306de2a82cc097db0e305a2083a682e37b6effa9
PaolaCuadros1 <paola.cuadroso@gmail.com>
2020-04-20 09:37:07 -0500

    Resulto ruta dinamica.

--- config/config.php
+++ config/config.php
@@ -3,5 +3,7 @@
  * Creación de una constante la cual nos guardará la ruta de nuestro proyecto
  */
 if ( !defined('ROOT')){
-  define("ROOT", 'http://'.$_SERVER['HTTP_HOST'].'/BIT/adminToStudent');
+  $root = str_replace('/opt/lampp/htdocs/', '/', __DIR__);
+  $root = str_replace('/config', '', $root);
+  define("ROOT", 'http://'.$_SERVER['HTTP_HOST'].$root);
 }
